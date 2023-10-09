e2fc67dade438a22288ddf92587952a71204b304
Brayan Ricaldi <brayan.r.cool@gmail.com>
2023-10-09 17:12:40 -0300

    Elimino target

--- git/target/debug/deps/git-4bcb309a8a25f7f3.d
+++
@@ -1,8 +0,0 @@
-/home/netsaj/Netsaj/FIUBA/Taller/TP-grupal/23C2-Rusteam-Visionary/git/target/debug/deps/git-4bcb309a8a25f7f3.rmeta: src/lib.rs src/errors.rs src/consts.rs src/config.rs
-
-/home/netsaj/Netsaj/FIUBA/Taller/TP-grupal/23C2-Rusteam-Visionary/git/target/debug/deps/git-4bcb309a8a25f7f3.d: src/lib.rs src/errors.rs src/consts.rs src/config.rs
-
-src/lib.rs:
-src/errors.rs:
-src/consts.rs:
-src/config.rs:
--- git/target/debug/deps/git-a63c039d83f5fe8d.d
+++
@@ -1,5 +0,0 @@
-/home/netsaj/Netsaj/FIUBA/Taller/TP-grupal/23C2-Rusteam-Visionary/git/target/debug/deps/git-a63c039d83f5fe8d.rmeta: src/main.rs
-
-/home/netsaj/Netsaj/FIUBA/Taller/TP-grupal/23C2-Rusteam-Visionary/git/target/debug/deps/git-a63c039d83f5fe8d.d: src/main.rs
-
-src/main.rs:
