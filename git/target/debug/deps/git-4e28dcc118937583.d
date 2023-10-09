ee2acfbba1e7cfee2cf0f04ce14acaee42953316
Brayan Ricaldi <brayan.r.cool@gmail.com>
2023-10-09 17:12:17 -0300

    Agrego ignore

---
+++ git/target/debug/deps/git-4e28dcc118937583.d
@@ -0,0 +1,8 @@
+/home/netsaj/Netsaj/FIUBA/Taller/TP-grupal/23C2-Rusteam-Visionary/git/target/debug/deps/git-4e28dcc118937583.rmeta: src/lib.rs src/errors.rs src/consts.rs src/config.rs
+
+/home/netsaj/Netsaj/FIUBA/Taller/TP-grupal/23C2-Rusteam-Visionary/git/target/debug/deps/git-4e28dcc118937583.d: src/lib.rs src/errors.rs src/consts.rs src/config.rs
+
+src/lib.rs:
+src/errors.rs:
+src/consts.rs:
+src/config.rs:
