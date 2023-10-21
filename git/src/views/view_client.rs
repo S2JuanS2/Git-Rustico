9dc833decaec8f5305da3082125774b280c61ec1
S2JuanS2 <juansdelrio@hotmail.com>
2023-10-21 17:05:45 -0300

    Agrego una pantalla de salida a la view

--- git/src/views/view_client.rs
+++ git/src/views/view_client.rs
@@ -1,6 +1,7 @@
 use crate::errors::GitError;
 use crate::controllers::controller_client::Controller;
 use gtk::prelude::*;
+use std::rc::Rc;
 
 pub struct View{
     controller: Controller,
@@ -27,21 +28,55 @@ impl View{
                 return Err(GitError::ObjectBuildFailed);
             }
         };
-        let button: gtk::Button = match builder.object("button1") {
-            Some(button) => button,
+        let button_clear: gtk::Button = match builder.object("button_clear") {
+            Some(button_clear) => button_clear,
             None => {
                 return Err(GitError::ObjectBuildFailed);
             }
         };
-        let entry: gtk::Entry = match builder.object("entry1") {
+        let button_send: gtk::Button = match builder.object("button_send") {
+            Some(button_send) => button_send,
+            None => {
+                return Err(GitError::ObjectBuildFailed);
+            }
+        };
+        let entry: gtk::Entry = match builder.object("entry_console") {
             Some(entry) => entry,
             None => {
                 return Err(GitError::ObjectBuildFailed);
             }
         };
-        button.connect_clicked(move |_| {
+
+        let response: Rc<gtk::TextView> = Rc::new(match builder.object("console") {
+            Some(response) => response,
+            None => {
+                return Err(GitError::ObjectBuildFailed);
+            }
+        });
+
+        let response_for_button_send = Rc::clone(&response);
+
+        button_send.connect_clicked(move |_| {
             let command = entry.text().to_string();
-            let _ = self.controller.send_command(command);
+            entry.set_text("");
+
+            let command_input = format!("{}\n\n",&command);
+            
+            let buffer = response_for_button_send.buffer().unwrap();
+            let mut end_iter = buffer.end_iter();
+            buffer.insert(&mut end_iter, &command_input);
+            
+            match self.controller.send_command(command){
+                Ok(_) => (),
+                Err(_) => return (),
+            };
+
+        });
+        button_clear.connect_clicked(move |_| {
+    
+            let buffer = response.buffer().unwrap();
+            buffer.set_text("");
+
         });
     
         window.show_all();
