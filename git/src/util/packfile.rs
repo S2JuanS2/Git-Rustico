2260c9693b302bd12a78d04cec5fda3917f9675f
Brayan Ricaldi <brayan.r.cool@gmail.com>
2023-10-23 11:47:36 -0300

    Agrego docu

--- git/src/util/packfile.rs
+++ git/src/util/packfile.rs
@@ -2,6 +2,7 @@ use std::io::Read;
 
 use crate::{consts::PACK_SIGNATURE, errors::GitError};
 
+
 pub fn read_packfile_header(reader: &mut dyn Read) -> Result<(), GitError> {
     read_signature(reader)?;
     println!("Signature: {}", PACK_SIGNATURE);
@@ -27,6 +28,20 @@ pub fn read_packfile_data(reader: &mut dyn Read) -> Result<(), GitError> {
     Ok(())
 }
 
+/// Lee y verifica la firma del encabezado del archivo PACKFILE a partir del lector proporcionado.
+///
+/// La firma es una secuencia de 4 bytes que debe coincidir con la firma esperada de Git.
+///
+/// # Argumentos
+///
+/// * `reader`: Un lector que implementa el trait `Read` para leer datos binarios.
+///
+/// # Retorno
+///
+/// Esta función no devuelve ningún valor útil en el éxito. En caso de éxito, significa que la
+/// firma del encabezado del archivo PACKFILE se leyó correctamente y coincide con la firma
+/// esperada. Si la firma no coincide o se produce un error de lectura, devuelve un error
+/// `GitError`.
 fn read_signature(reader: &mut dyn Read) -> Result<(), GitError> {
     let mut buffer = [0u8; 4];
     if reader.read_exact(&mut buffer).is_err() {
@@ -39,6 +54,18 @@ fn read_signature(reader: &mut dyn Read) -> Result<(), GitError> {
     Ok(())
 }
 
+/// Lee y verifica la versión del encabezado del archivo PACKFILE a partir del lector proporcionado.
+///
+/// La versión es un número de 4 bytes que debe ser 2 o 3 para ser compatible con el formato del archivo PACKFILE de Git.
+///
+/// # Argumentos
+///
+/// * `reader`: Un lector que implementa el trait `Read` para leer datos binarios.
+///
+/// # Retorno
+///
+/// En caso de éxito, devuelve el valor de la versión del archivo PACKFILE leído. Si la versión
+/// no es 2 o 3, o se produce un error de lectura, se devuelve un error `GitError`.
 fn read_version(reader: &mut dyn Read) -> Result<u32, GitError> {
     let mut buffer = [0u8; 4];
     if reader.read_exact(&mut buffer).is_err() {
@@ -53,6 +80,17 @@ fn read_version(reader: &mut dyn Read) -> Result<u32, GitError> {
     Ok(version)
 }
 
+/// Lee y verifica el número de objetos contenidos en el encabezado del archivo PACKFILE a partir del lector proporcionado.
+///
+/// Este número es un valor de 4 bytes que representa la cantidad de objetos en el archivo PACKFILE.
+///
+/// # Argumentos
+///
+/// * `reader`: Un lector que implementa el trait `Read` para leer datos binarios.
+///
+/// # Retorno
+///
+/// En caso de éxito, devuelve el número de objetos contenidos en el archivo PACKFILE leído. En caso de error de lectura, se devuelve un error `GitError`.
 fn read_objects_contained(reader: &mut dyn Read) -> Result<u32, GitError> {
     let mut buffer = [0; 4];
     if reader.read_exact(&mut buffer).is_err() {
