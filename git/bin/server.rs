8581aebda9fc334e77857fff077ced075619ac60
Brayan Ricaldi <brayan.r.cool@gmail.com>
2023-10-25 12:33:57 -0300

    Agrego write_log_file

--- git/bin/server.rs
+++ git/bin/server.rs
@@ -1,9 +1,13 @@
 use git::config::Config;
 use git::errors::GitError;
 use git::util::connections::start_server;
-use std::env;
+use std::fs::OpenOptions;
+use std::sync::mpsc::{self, Sender, Receiver};
+use std::thread::JoinHandle;
+use std::{env, thread};
 use std::io::Read;
-use std::net::TcpStream;
+use std::net::{TcpStream, TcpListener};
+use std::io::Write;
 
 fn handle_client(mut stream: TcpStream) {
     // Leer datos del cliente
@@ -27,19 +31,53 @@ fn main() -> Result<(), GitError> {
     let listener = start_server(&address)?;
     println!("Servidor escuchando en {}", address);
 
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let _ = write_log_file(&config.path_log, rx);
+    });
+
+    thread::spawn(move || {
+        let _ = receive_client(&listener, tx);
+    });
+
+
+    Ok(())
+}
+
+fn receive_client(listener: & TcpListener, tx: Sender<String>) -> Result<Vec<JoinHandle<()>>, GitError> {
+    let _ = tx;
+    let mut handles: Vec<JoinHandle<()>> = vec![];
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("Nueva conexión: {:?}", stream.local_addr());
-                std::thread::spawn(|| {
+                handles.push(std::thread::spawn(|| {
                     handle_client(stream);
-                });
+                }));
             }
             Err(e) => {
                 eprintln!("Error al aceptar la conexión: {}", e);
             }
         }
     }
+    Ok(handles)
+}
+
+fn write_log_file(log_path: &str, rx: Receiver<String>) -> Result<(), std::io::Error> {
+    // Intentamos abrir el archivo en modo append o crearlo si no existe.
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(log_path)?;
+
+    // Creamos un bucle para recibir datos del canal y escribirlos en el archivo.
+    for received_data in rx {
+        writeln!(file, "{}", received_data)?;
+    }
+
+    // Cerramos el archivo al finalizar.
+    file.sync_all()?;
 
     Ok(())
 }
--- git/src/util/pkt_line.rs
+++ git/src/util/pkt_line.rs
@@ -1,6 +1,7 @@
 use std::io::Read;
 
-use crate::{consts::LENGTH_PREFIX_SIZE, errors::GitError};
+use crate::{errors::GitError, consts::LENGTH_PREFIX_SIZE};
+
 
 /// Lee líneas de paquete del flujo de entrada proporcionado y las devuelve como un vector de vectores de bytes.
 ///
@@ -37,6 +38,7 @@ pub fn read(stream: &mut dyn Read) -> Result<Vec<Vec<u8>>, GitError> {
     }
 }
 
+
 /// Lee una línea de paquete del flujo de entrada proporcionado y la devuelve como un vector de bytes.
 ///
 /// Esta función espera que el flujo de entrada contenga líneas de paquete según el formato de Git:
@@ -58,80 +60,41 @@ pub fn read(stream: &mut dyn Read) -> Result<Vec<Vec<u8>>, GitError> {
 /// # Retorno
 ///
 /// - `Result<Vec<u8>, GitError>`: Un resultado que contiene el contenido de la línea de paquete o un error si ocurre alguno.
-fn read_pkt_line(socket: &mut dyn Read) -> Result<Vec<u8>, GitError> {
+fn read_pkt_line(socket: &mut dyn Read) -> Result<Vec<u8>, GitError>
+{
     let mut length_buf = [0u8; 4];
-    if socket.read_exact(&mut length_buf).is_err() {
+    if socket.read_exact(&mut length_buf).is_err()
+    {
         return Err(GitError::InvalidPacketLineError);
     };
 
     let length_hex = String::from_utf8_lossy(&length_buf);
-    let length = match u32::from_str_radix(length_hex.trim(), 16) {
+    let length = match u32::from_str_radix(length_hex.trim(), 16)
+    {
         Ok(l) => l,
-        Err(_) => return Err(GitError::InvalidPacketLineError),
+        Err(_) => return Err(GitError::InvalidPacketLineError), 
     };
 
     if length == 0 {
         // End of the packet
         return Ok(vec![]);
     }
-
+    
     let length = length as usize - LENGTH_PREFIX_SIZE - 1; // 1 por el enter
-    let mut content = vec![0u8; length];
-    if socket.read_exact(&mut content).is_err() {
+    let mut content = vec![0u8; length as usize];
+    if socket.read_exact(&mut content).is_err()
+    {
         return Err(GitError::InvalidPacketLineError);
     };
 
     // Consume the newline character
     let mut newline_buf = [0u8; 1];
-    if socket.read_exact(&mut newline_buf).is_err() {
+    if socket.read_exact(&mut newline_buf).is_err()
+    {
         return Err(GitError::InvalidPacketLineError);
     };
 
     Ok(content)
-}
-
-/// Agrega un prefijo de longitud a un mensaje y lo devuelve como una cadena.
-///
-/// Esta función toma un mensaje y la longitud del mensaje sin el prefijo y agrega un
-/// prefijo hexadecimal de 4 caracteres que indica la longitud del mensaje completo.
-///
-/// # Argumentos
-///
-/// * `message`: Un mensaje al que se le agregará el prefijo de longitud.
-/// * `len`: La longitud del mensaje original (sin el prefijo), el motivo de este argumento
-///     es porque algunas cadenas contienen \0 y la función `len()` no funciona correctamente.
-///
-/// # Ejemplo
-///
-/// ```
-/// use git::util::pkt_line::add_length_prefix;
-///
-/// let message = "Hola, mundo!";
-/// let length = message.len();
-///
-/// let prefixed_message = add_length_prefix(message, length);
-///
-/// // El resultado será una cadena con el prefijo de longitud:
-/// // "0010Hola, mundo!"
-/// ```
-///
-/// # Retorno
-///
-/// Devuelve una cadena que contiene el mensaje original con un prefijo de longitud hexadecimal.
-///
-/// Nota: El prefijo de longitud se calcula automáticamente y tiene una longitud fija de 4 caracteres.
-///
-pub fn add_length_prefix(message: &str, len: usize) -> String {
-    // Obtener la longitud del mensaje con el prefijo
-    let message_length = len + LENGTH_PREFIX_SIZE;
-
-    // Convertir la longitud en una cadena hexadecimal de 4 caracteres
-    let length_hex = format!("{:04x}", message_length);
-
-    // Concatenar la longitud al principio del mensaje
-    let prefixed_message = format!("{}{}", length_hex, message);
-
-    prefixed_message
 }
 
 #[cfg(test)]
@@ -223,26 +186,5 @@ mod tests {
         let result = read(&mut stream);
 
         assert!(result.is_err());
-    }
-
-    #[test]
-    fn test_add_length_prefix() {
-        let message = "7217a7c7e582c46cec22a130adf4b9d7d950fba0 HEAD\0multi_ack thin-pack side-band side-band-64k ofs-delta shallow no-progress include-tag\n";
-
-        let prefixed_message = add_length_prefix(message, 132);
-
-        assert_eq!(prefixed_message, "00887217a7c7e582c46cec22a130adf4b9d7d950fba0 HEAD\0multi_ack thin-pack side-band side-band-64k ofs-delta shallow no-progress include-tag\n");
-    }
-
-    #[test]
-    fn test_add_length_prefix_incorrect() {
-        let message = "Hola, mundo!";
-        let length = message.len();
-
-        // Le quito 1 al len para que me de un prefijo incorrecto
-        let prefixed_message = add_length_prefix(message, length - 1);
-
-        // assert_eq!(prefixed_message, incorrect_reference);
-        assert_ne!(prefixed_message, "0010Hola, mundo!");
     }
 }
