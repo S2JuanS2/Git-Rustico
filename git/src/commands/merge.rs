aba0264ebd7f9fc7daacb36946c40221b942eda8
valenlanzillotta <vlanzillotta@fi.uba.ar>
2023-11-03 20:35:45 -0300

    fast-forward andando

--- git/src/commands/merge.rs
+++ git/src/commands/merge.rs
@@ -3,6 +3,7 @@ use crate::errors::GitError;
 use crate::models::client::Client;
 use crate::util::files::create_file_replace;
 use std::fs::File;
+use std::fs;
 use std::io::{Read, Write};
 
 use super::branch::get_current_branch;
@@ -29,13 +30,9 @@ pub fn handle_merge(args: Vec<&str>, client: Client) -> Result<(), GitError> {
 /// 'branch_name': nombre de la rama a mergear
 pub fn git_merge(directory: &str, branch_name: &str) -> Result<(), GitError> {
     let current_branch = get_current_branch(directory)?;
-    println!("current_branch: {}", current_branch);
 
     let path_current_branch = format!("{}/.git/refs/heads/{}", directory, current_branch);
     let path_branch_to_merge = format!("{}/.git/refs/heads/{}", directory, branch_name);
-
-    println!("path_current_branch: {}", path_current_branch);
-    println!("path_branch_to_merge: {}", path_branch_to_merge);
 
     let mut current_branch_file = match File::open(&path_current_branch){
         Ok(file) => file,
@@ -46,7 +43,6 @@ pub fn git_merge(directory: &str, branch_name: &str) -> Result<(), GitError> {
         Ok(file) => file,
         Err(_) => return Err(GitError::ReadFileError),
     };
-    println!("current_branch_hash: {}", current_branch_hash);
 
     let mut merge_branch_file = match File::open(path_branch_to_merge){
         Ok(file) => file,
@@ -57,34 +53,20 @@ pub fn git_merge(directory: &str, branch_name: &str) -> Result<(), GitError> {
         Ok(file) => file,
         Err(_) => return Err(GitError::ReadFileError),
     };
-    println!("branch_to_merge_hash: {}", branch_to_merge_hash);
 
     if current_branch_hash == branch_to_merge_hash || current_branch_hash == branch_name{
         println!("Already up to date.");
         return Ok(());
     }
 
-
-    // FORMATO LOG
-    // Commit: 1a9bdce57b744e76f8fb7d8bbd0a272bbcd6e482
-    // Author: Valen <vlanzillotta@fi.uba.ar>
-    // Commit: 0a56b3675a740a84ad190edfacfecf17d0d4edea
-    // Author: Juan <jdr@fi.uba.ar>
-
-
     else {
         let log_current_branch = git_log(directory)?;
         let log_current_branch = get_commits_from_log(log_current_branch);
-        println!("log_current_branch: {:?}", log_current_branch);
         git_checkout_switch(directory, branch_name)?;
-        println!("On branch {}", get_current_branch(directory)?);
         let log_merge_branch = git_log(directory)?;
         let log_merge_branch = get_commits_from_log(log_merge_branch);
         
-
-        println!("log_merge_branch: {:?}", log_merge_branch);
         git_checkout_switch(directory, &current_branch)?;
-        println!("On branch {}", get_current_branch(directory)?);
         
         for commit in log_current_branch.iter() {
             if let Some(last_hash_merge_branch) = log_merge_branch.last() {
@@ -98,38 +80,60 @@ pub fn git_merge(directory: &str, branch_name: &str) -> Result<(), GitError> {
         let first_commit_merge_branch = &log_merge_branch[0];
         let root_parent_current_branch = git_cat_file(directory, first_commit_current_branch, "-p")?;
         let root_parent_merge_branch = git_cat_file(directory, first_commit_merge_branch, "-p")?;
-        let mut hash_parent = "0000000000000000000000000000000000000000";
+        let mut hash_parent_current = "0000000000000000000000000000000000000000";
+        let mut hash_parent_merge = "0000000000000000000000000000000000000000";
         for line in root_parent_current_branch.lines() {
             if line.starts_with("parent ") {
                 if let Some(hash) = line.strip_prefix("parent ") {
-                    if hash != "0000000000000000000000000000000000000000" {
-                        hash_parent = hash;
-                    }
+                    hash_parent_current = hash;
                 }
             }
         }
-        if hash_parent == "0000000000000000000000000000000000000000" {
-            for line in root_parent_merge_branch.lines() {
-                if line.starts_with("parent ") {
-                    if let Some(hash) = line.strip_prefix("parent ") {
-                        hash_parent = hash;
-                    }
+        for line in root_parent_merge_branch.lines() {
+            if line.starts_with("parent ") {
+                if let Some(hash) = line.strip_prefix("parent ") {
+                    hash_parent_merge = hash;
                 }
             }
         }
-        println!("hash_parent: {}", hash_parent);
-
-        fast_forward(path_current_branch, branch_to_merge_hash, current_branch_hash, log_merge_branch, hash_parent, directory)?;
-        // for file in modified_files {
-        //     println!("{}", file);
-        // }
-        // me falta agregar los files de branch_to_merge en current_branch
+        if hash_parent_current == hash_parent_merge {
+            // recursive strategy
+        }
+        else {
+            fast_forward(path_current_branch, branch_to_merge_hash, current_branch_hash, log_merge_branch, directory, branch_name)?;
+        }
     }
 
     Ok(())
 }
 
-fn fast_forward(path_current_branch: String, branch_to_merge_hash: String, current_branch_hash: String, log_merge_branch: Vec<String>, hash_parent: &str, directory: &str) -> Result<(), GitError> {
+fn fast_forward(path_current_branch: String, branch_to_merge_hash: String, current_branch_hash: String, log_merge_branch: Vec<String>, directory: &str, branch_to_merge: &str) -> Result<(), GitError> {
+    for commit in log_merge_branch {
+        let content_commit = git_cat_file(directory, &commit, "-p")?;
+
+        let mut content_tree = String::new();
+        for line in content_commit.lines() {
+            if line.starts_with("tree ") {
+                if let Some(hash) = line.strip_prefix("tree ") {
+                    let hash_tree_in_commit = hash;
+                    content_tree = git_cat_file(directory, hash_tree_in_commit, "-p")?;
+                }
+            }
+        }
+        for line in content_tree.lines() {
+            let file = line.split_whitespace().skip(1).take(1).collect::<String>();
+            let hash_blob = line.split_whitespace().skip(2).take(1).collect::<String>();
+            let content_file = git_cat_file(directory, &hash_blob, "-p")?;
+            let path_file_format = format!("{}/{}", directory, file);
+            if let Ok(metadata) = fs::metadata(&path_file_format) {
+                if metadata.is_file() {
+                    compare_files(&path_file_format, &content_file, branch_to_merge)?;
+                }
+            } else {
+                create_file_replace(&path_file_format, &content_file)?;
+            };
+        }
+    }
     let mut file_current_branch = match File::create(&path_current_branch){
         Ok(file) => file,
         Err(_) => return Err(GitError::CreateFileError),
@@ -140,39 +144,29 @@ fn fast_forward(path_current_branch: String, branch_to_merge_hash: String, curre
     };
     println!("Updating {}..{}", current_branch_hash, branch_to_merge_hash);
     println!("Fast-forward");
-    Ok(for commit in log_merge_branch {
-    // tengo q hacer esto con cada commit hasta llegar al hash_parent
-        if commit == hash_parent {
-            break;
-        }
-        let content_commit = git_cat_file(directory, &commit, "-p")?;
-        println!("content_commit:\n{}", content_commit);
+    Ok(())
+}
 
-        let mut content_tree = String::new();
-        for line in content_commit.lines() {
-            if line.starts_with("tree ") {
-                if let Some(hash) = line.strip_prefix("tree ") {
-                    let hash_tree_in_commit = hash;
-                    println!("hash_tree_in_commit: {}", hash_tree_in_commit);
-                    content_tree = git_cat_file(directory, hash_tree_in_commit, "-p")?;
-                }
-            }
-        }
-        // let mut modified_files: Vec<&str> = Vec::new();
-        println!("content_tree:\n{}", content_tree);
-        for line in content_tree.lines() {
-            let file = line.split_whitespace().skip(1).take(1).collect::<String>();
-            println!("file: {}", file);
-            let hash_blob = line.split_whitespace().skip(2).take(1).collect::<String>();
-            println!("hash_blob: {}", hash_blob);
-            let content_file = git_cat_file(directory, &hash_blob, "-p")?;
-            println!("content_file:\n{}", content_file);
-            let path_file_format = format!("{}/{}", directory, file);
-            println!("path_file_format: {}", path_file_format);
-            create_file_replace(&path_file_format, &content_file)?;
-            // modified_files.push(&file);
-        }
-    })
+fn compare_files(path_file_format: &str, content_file: &str, branch_to_merge: &str) -> Result<(), GitError> {
+    let mut file = match File::open(&path_file_format){
+        Ok(file) => file,
+        Err(_) => return Err(GitError::OpenFileError),
+    };
+    let mut content_file_local: String = String::new();
+    match file.read_to_string(&mut content_file_local){
+        Ok(file) => file,
+        Err(_) => return Err(GitError::ReadFileError),
+    };
+    if content_file_local != content_file {
+        // CONFLICTO
+        check_each_line(path_file_format, content_file_local, content_file, branch_to_merge)?;
+        println!("Auto-merging {}\n", path_file_format);
+        println!("CONFLICT (content): Merge conflict in {}\n", path_file_format);
+        println!("Automatic merge failed; fix conflicts and then commit the result.\n");
+        return Ok(());
+    }
+    create_file_replace(path_file_format, content_file)?;
+    Ok(())
 }
 
 /// Esta función se encarga de obtener los commits de un log
@@ -191,6 +185,40 @@ fn get_commits_from_log(log: String) -> Vec<String> {
     commit_hashes
 }
 
+fn check_each_line(path_file_format: &str, content_file_local: String, content_file: &str, branch_to_merge: &str) -> Result<(), GitError> {
+    let mut content_file_local_lines = content_file_local.lines();
+    let mut content_file_lines = content_file.lines();
+    let mut line_local = content_file_local_lines.next();
+    let mut line = content_file_lines.next();
+
+    let mut new_content_file = String::new();
+
+    while line_local != None && line != None {
+        if line_local != line {
+            new_content_file.push_str("<<<<<<< HEAD\n");
+            if let Some(line_local) = line_local {
+                new_content_file.push_str(line_local);
+            }
+            new_content_file.push_str("\n=======\n");
+            if let Some(line) = line {
+                new_content_file.push_str(line);
+            }
+            new_content_file.push_str("\n>>>>>>> ");
+            new_content_file.push_str(branch_to_merge);
+            new_content_file.push_str("\n");
+        }
+        else {
+            if let Some(line_local) = line_local {
+                new_content_file.push_str(line_local);
+            }
+            new_content_file.push_str("\n");
+        }
+        line_local = content_file_local_lines.next();
+        line = content_file_lines.next();
+    }
+    create_file_replace(path_file_format, &new_content_file)?;
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
@@ -217,7 +245,6 @@ mod tests {
         let mut file2 = fs::File::create(&file_path2).expect("Falló al crear el archivo");
         file2.write_all(b"Otro archivo a commitear")
             .expect("Error al escribir en el archivo");
-
 
         let file_path3 = format!("{}/{}", directory, "tocommitinnew2.txt");
         let mut file = fs::File::create(&file_path3).expect("Falló al crear el archivo");
