7b2263a1eb966c4891f0bac43e3b79cc8539c45f
valenlanzillotta <vlanzillotta@fi.uba.ar>
2023-11-03 13:49:14 -0300

    fast-forward anda

--- git/src/commands/merge.rs
+++ git/src/commands/merge.rs
@@ -1,6 +1,7 @@
 use crate::commands::cat_file::git_cat_file;
 use crate::errors::GitError;
 use crate::models::client::Client;
+use crate::util::files::create_file_replace;
 use std::fs::File;
 use std::io::{Read, Write};
 
@@ -93,40 +94,32 @@ pub fn git_merge(directory: &str, branch_name: &str) -> Result<(), GitError> {
                 }
             }
         }
-        // let last_hash_in_common = find_hash_in_common(&log_current_branch, &log_merge_branch)?;
-        // println!("last_hash_in_common: {}", last_hash_in_common);
-        // Actualizo la rama actual con su nuevo commit.
-        let mut file_current_branch = match File::create(&path_current_branch){
-            Ok(file) => file,
-            Err(_) => return Err(GitError::CreateFileError),
-        };
-        match file_current_branch.write_all(branch_to_merge_hash.as_bytes()) {
-            Ok(_) => (),
-            Err(_) => return Err(GitError::WriteFileError),
-        };
-        println!("Updating {}..{}", current_branch_hash, branch_to_merge_hash);
-        println!("Fast-forward");
-        // let mut modified_files: Vec<&str> = Vec::new();
-        let content_commit = git_cat_file(directory, &branch_to_merge_hash, "-p")?;
-        println!("content_commit:\n{}", content_commit);
-        for line in content_commit.lines() {
-            if line.starts_with("tree ") {
-                if let Some(hash) = line.strip_prefix("tree ") {
-                    let hash_tree_in_commit = hash;
-                    println!("hash_tree_in_commit: {}", hash_tree_in_commit);
-                    let content_tree = git_cat_file(directory, hash_tree_in_commit, "-p")?;
-                    println!("content_tree:\n{}", content_tree);
+        let first_commit_current_branch = &log_current_branch[0];
+        let first_commit_merge_branch = &log_merge_branch[0];
+        let root_parent_current_branch = git_cat_file(directory, first_commit_current_branch, "-p")?;
+        let root_parent_merge_branch = git_cat_file(directory, first_commit_merge_branch, "-p")?;
+        let mut hash_parent = "0000000000000000000000000000000000000000";
+        for line in root_parent_current_branch.lines() {
+            if line.starts_with("parent ") {
+                if let Some(hash) = line.strip_prefix("parent ") {
+                    if hash != "0000000000000000000000000000000000000000" {
+                        hash_parent = hash;
+                    }
                 }
             }
         }
-        // let hash_tree_in_commit = content_commit.split(' ').collect::<Vec<&str>>()[1];
-        // println!("hash_tree_in_commit: {}", hash_tree_in_commit);
-        // let content_tree = git_cat_file(directory, hash_tree_in_commit, "-p")?;
-        // println!("content_tree:\n{}", content_tree);
-        // for line in content_tree.lines() {
-        //     let file = line.split(' ').collect::<Vec<&str>>()[3];
-        //     modified_files.push(file);
-        // }
+        if hash_parent == "0000000000000000000000000000000000000000" {
+            for line in root_parent_merge_branch.lines() {
+                if line.starts_with("parent ") {
+                    if let Some(hash) = line.strip_prefix("parent ") {
+                        hash_parent = hash;
+                    }
+                }
+            }
+        }
+        println!("hash_parent: {}", hash_parent);
+
+        fast_forward(path_current_branch, branch_to_merge_hash, current_branch_hash, log_merge_branch, hash_parent, directory)?;
         // for file in modified_files {
         //     println!("{}", file);
         // }
@@ -134,6 +127,52 @@ pub fn git_merge(directory: &str, branch_name: &str) -> Result<(), GitError> {
     }
 
     Ok(())
+}
+
+fn fast_forward(path_current_branch: String, branch_to_merge_hash: String, current_branch_hash: String, log_merge_branch: Vec<String>, hash_parent: &str, directory: &str) -> Result<(), GitError> {
+    let mut file_current_branch = match File::create(&path_current_branch){
+        Ok(file) => file,
+        Err(_) => return Err(GitError::CreateFileError),
+    };
+    match file_current_branch.write_all(branch_to_merge_hash.as_bytes()) {
+        Ok(_) => (),
+        Err(_) => return Err(GitError::WriteFileError),
+    };
+    println!("Updating {}..{}", current_branch_hash, branch_to_merge_hash);
+    println!("Fast-forward");
+    Ok(for commit in log_merge_branch {
+    // tengo q hacer esto con cada commit hasta llegar al hash_parent
+        if commit == hash_parent {
+            break;
+        }
+        let content_commit = git_cat_file(directory, &commit, "-p")?;
+        println!("content_commit:\n{}", content_commit);
+
+        let mut content_tree = String::new();
+        for line in content_commit.lines() {
+            if line.starts_with("tree ") {
+                if let Some(hash) = line.strip_prefix("tree ") {
+                    let hash_tree_in_commit = hash;
+                    println!("hash_tree_in_commit: {}", hash_tree_in_commit);
+                    content_tree = git_cat_file(directory, hash_tree_in_commit, "-p")?;
+                }
+            }
+        }
+        // let mut modified_files: Vec<&str> = Vec::new();
+        println!("content_tree:\n{}", content_tree);
+        for line in content_tree.lines() {
+            let file = line.split_whitespace().skip(1).take(1).collect::<String>();
+            println!("file: {}", file);
+            let hash_blob = line.split_whitespace().skip(2).take(1).collect::<String>();
+            println!("hash_blob: {}", hash_blob);
+            let content_file = git_cat_file(directory, &hash_blob, "-p")?;
+            println!("content_file:\n{}", content_file);
+            let path_file_format = format!("{}/{}", directory, file);
+            println!("path_file_format: {}", path_file_format);
+            create_file_replace(&path_file_format, &content_file)?;
+            // modified_files.push(&file);
+        }
+    })
 }
 
 /// Esta función se encarga de obtener los commits de un log
@@ -152,21 +191,6 @@ fn get_commits_from_log(log: String) -> Vec<String> {
     commit_hashes
 }
 
-/// Esta función se encarga de encontrar el hash en común entre dos logs de commits
-/// ###Parametros:
-/// 'log_current_branch': Vector de strings que contiene los hashes de los commits de la rama actual
-/// 'log_merge_branch': Vector de strings que contiene los hashes de los commits de la rama a mergear
-// fn find_hash_in_common(log_current_branch: &Vec<String>, log_merge_branch: &Vec<String>) -> Result<String, GitError> {
-//     for commit_current in log_current_branch {
-//         for commit_merge in log_merge_branch {
-//             if commit_current == commit_merge {
-//                 println!("Hash in common: {}", commit_current);
-//                 return Ok(commit_current.to_string())
-//             }
-//         }
-//     }
-//     Err(GitError::HashInCommonNotFoundError)
-// }
 
 #[cfg(test)]
 mod tests {
@@ -256,7 +280,7 @@ mod tests {
         git_commit(directory, test_commit2).expect("Error al commitear");
 
         git_checkout_switch(directory, "main").expect("Error al cambiar de rama");
-                
+
         git_merge(directory, "new_branch").expect("Error al mergear");
 
         fs::remove_dir_all(directory).expect("Falló al remover el directorio temporal");
