53d4373a610193ce968470a44ecc043d50b19a8b
valenlanzillotta <vlanzillotta@fi.uba.ar>
2023-11-04 21:19:26 -0300

    refactor

--- git/src/commands/merge.rs
+++ git/src/commands/merge.rs
@@ -1,9 +1,7 @@
 use crate::errors::GitError;
 use crate::models::client::Client;
 use crate::util::files::{create_file_replace, open_file, read_file_string};
-use std::fs::File;
 use std::fs;
-use std::io::Read;
 
 use super::branch::get_current_branch;
 use super::checkout::git_checkout_switch;
@@ -42,16 +40,10 @@ pub fn git_merge(directory: &str, branch_name: &str) -> Result<String, GitError>
         formatted_result.push_str("Already up to date.");
         return Ok(formatted_result);
     }
-
     else {
-        let log_current_branch = git_log(directory)?;
-        let log_current_branch = get_commits_from_log(log_current_branch);
-        git_checkout_switch(directory, branch_name)?;
-        let log_merge_branch = git_log(directory)?;
-        let log_merge_branch = get_commits_from_log(log_merge_branch);
-        git_checkout_switch(directory, &current_branch)?;
-        
+        let (log_current_branch, log_merge_branch) = get_logs_from_branches(directory, branch_name, current_branch)?;
         check_if_current_is_up_to_date(&log_current_branch, &log_merge_branch, &mut formatted_result);
+
         let first_commit_current_branch = &log_current_branch[0];
         let first_commit_merge_branch = &log_merge_branch[0];
         let root_parent_current_branch = git_cat_file(directory, first_commit_current_branch, "-p")?;
@@ -60,10 +52,32 @@ pub fn git_merge(directory: &str, branch_name: &str) -> Result<String, GitError>
         let strategy = merge_depending_on_strategy(&hash_parent_current, &hash_parent_merge, log_merge_branch, directory, branch_name)?;
         get_result_depending_on_strategy(strategy, &mut formatted_result, current_branch_hash, branch_to_merge_hash, path_current_branch)?;
     }
-
     Ok(formatted_result)
 }
 
+/// Obtiene los logs de las ramas a mergear
+/// ###Parametros:
+/// 'directory': directorio del repositorio local
+/// 'branch_name': nombre de la rama a mergear
+/// 'current_branch': nombre de la rama actual
+fn get_logs_from_branches(directory: &str, branch_name: &str, current_branch: String) -> Result<(Vec<String>, Vec<String>), GitError> {
+    let log_current_branch = git_log(directory)?;
+    let log_current_branch = get_commits_from_log(log_current_branch);
+    git_checkout_switch(directory, branch_name)?;
+    let log_merge_branch = git_log(directory)?;
+    let log_merge_branch = get_commits_from_log(log_merge_branch);
+    git_checkout_switch(directory, &current_branch)?;
+    Ok((log_current_branch, log_merge_branch))
+}
+
+/// Obtiene el resultado del merge dependiendo de la estrategia de merge utilizada
+/// ###Parametros:
+/// 'strategy': tupla que contiene la estrategia de merge utilizada y el archivo en conflicto (o ok si no hay
+/// conflictos)
+/// 'formatted_result': String que contiene el resultado formateado del merge.
+/// 'current_branch_hash': hash del commit de la rama actual
+/// 'branch_to_merge_hash': hash del commit de la rama a mergear
+/// 'path_current_branch': path del archivo de la rama actual
 fn get_result_depending_on_strategy(strategy: (String, String), formatted_result: &mut String, current_branch_hash: String, branch_to_merge_hash: String, path_current_branch: String) -> Result<(), GitError> {
     Ok(if strategy.0 == "recursive" && strategy.1 == "ok" {
         formatted_result.push_str("Merge made by the 'recursive' strategy.");
@@ -81,6 +95,10 @@ fn get_result_depending_on_strategy(strategy: (String, String), formatted_result
     })
 }
 
+/// Obtiene los hashes de los padres de los commits de las ramas a mergear
+/// ###Parametros:
+/// 'root_parent_current_branch': String que contiene el contenido del primer commit de la rama actual
+/// 'root_parent_merge_branch': String que contiene el contenido del primer commit de la rama a mergear
 fn get_parent_hashes(root_parent_current_branch: String, root_parent_merge_branch: String) -> (String, String) {
     let mut hash_parent_current = "0000000000000000000000000000000000000000";
     let mut hash_parent_merge = "0000000000000000000000000000000000000000";
@@ -117,20 +135,18 @@ fn check_if_current_is_up_to_date(log_current_branch: &Vec<String>, log_merge_br
     }
 }
 
+/// Recorre los commits en la merge branch y hace el merge dependiendo de la estrategia a utilizar.
+/// ###Parametros:
+/// 'hash_parent_current': hash del padre del commit de la rama actual
+/// 'hash_parent_merge': hash del padre del commit de la rama a mergear
+/// 'log_merge_branch': Vector de strings que contiene los commits de la rama a mergear.
+/// 'directory': directorio del repositorio local
+/// 'branch_to_merge': nombre de la rama a mergear
 fn merge_depending_on_strategy(hash_parent_current: &str, hash_parent_merge: &str, log_merge_branch: Vec<String>, directory: &str, branch_to_merge: &str) -> Result<(String, String), GitError> {
     let mut strategy: (String, String) = ("".to_string(), "".to_string());
     for commit in log_merge_branch {
         let content_commit = git_cat_file(directory, &commit, "-p")?;
-
-        let mut content_tree = String::new();
-        for line in content_commit.lines() {
-            if line.starts_with("tree ") {
-                if let Some(hash) = line.strip_prefix("tree ") {
-                    let hash_tree_in_commit = hash;
-                    content_tree = git_cat_file(directory, hash_tree_in_commit, "-p")?;
-                }
-            }
-        }
+        let content_tree = get_tree_of_commit(content_commit, directory)?;
         for line in content_tree.lines() {
             let file = line.split_whitespace().skip(1).take(1).collect::<String>();
             let hash_blob = line.split_whitespace().skip(2).take(1).collect::<String>();
@@ -159,16 +175,32 @@ fn merge_depending_on_strategy(hash_parent_current: &str, hash_parent_merge: &st
     Ok(strategy)
 }
 
+/// Obtiene el contenido del arbol del commit
+/// ###Parametros:
+/// 'content_commit': String que contiene el contenido del commit
+/// 'directory': directorio del repositorio local
+fn get_tree_of_commit(content_commit: String, directory: &str) -> Result<String, GitError> {
+    let mut content_tree = String::new();
+    for line in content_commit.lines() {
+        if line.starts_with("tree ") {
+            if let Some(hash) = line.strip_prefix("tree ") {
+                let hash_tree_in_commit = hash;
+                content_tree = git_cat_file(directory, hash_tree_in_commit, "-p")?;
+            }
+        }
+    }
+    Ok(content_tree)
+}
+
+/// Compara el archivo en la rama actual con el de la rama a mergear
+/// ###Parametros:
+/// 'path_file_format': path del archivo a comparar
+/// 'content_file': contenido del archivo en la rama a mergear
+/// 'branch_to_merge': nombre de la rama a mergear
+/// 'strategy': tupla que contiene la estrategia de merge utilizada y el archivo en conflicto (u ok si no hay)
 fn compare_files(path_file_format: &str, content_file: &str, branch_to_merge: &str, strategy: &mut (String, String)) -> Result<(), GitError> {
-    let mut file = match File::open(&path_file_format){
-        Ok(file) => file,
-        Err(_) => return Err(GitError::OpenFileError),
-    };
-    let mut content_file_local: String = String::new();
-    match file.read_to_string(&mut content_file_local){
-        Ok(file) => file,
-        Err(_) => return Err(GitError::ReadFileError),
-    };
+    let file = open_file(&path_file_format)?;
+    let content_file_local = read_file_string(file)?;
     if content_file_local != content_file {
         // CONFLICTO
         let path_conflict = check_each_line(path_file_format, content_file_local, content_file, branch_to_merge)?;
@@ -200,6 +232,12 @@ fn get_commits_from_log(log: String) -> Vec<String> {
     commit_hashes
 }
 
+/// Chequea cada linea de los archivos para ver si hay conflictos
+/// ###Parametros:
+/// 'path_file_format': path del archivo a comparar
+/// 'content_file_local': contenido del archivo en la rama actual
+/// 'content_file': contenido del archivo en la rama a mergear
+/// 'branch_to_merge': nombre de la rama a mergear
 fn check_each_line(path_file_format: &str, content_file_local: String, content_file: &str, branch_to_merge: &str) -> Result<String, GitError> {
     let mut content_file_local_lines = content_file_local.lines();
     let mut content_file_lines = content_file.lines();
