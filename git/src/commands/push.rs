cf467ed3234cc352375cde4a4a5863b4ce3cc84c
Brayan Ricaldi <brayan.r.cool@gmail.com>
2023-12-01 00:10:42 -0300

    First encounter with push

--- git/src/commands/push.rs
+++ git/src/commands/push.rs
@@ -1,9 +1,12 @@
 use super::errors::CommandsError;
 use crate::commands::config::GitConfig;
-use crate::git_transport::references::Reference;
+use crate::git_transport::git_request::GitRequest;
+use crate::git_transport::references::{Reference, reference_discovery};
+use crate::git_transport::request_command::RequestCommand;
 use crate::models::client::Client;
 use crate::util::connections::start_client;
 use std::net::TcpStream;
+use std::path::Path;
 
 /// Maneja el comando "push" en el servidor Git.
 ///
@@ -43,9 +46,9 @@ pub fn handle_push(args: Vec<&str>, client: Client) -> Result<String, CommandsEr
 /// 'branch_name': nombre de la rama a mergear
 // Quiero actualizar mi branch actual con los cambios del repositorio remoto
 pub fn git_push(
-    _socket: &mut TcpStream,
-    _ip: &str,
-    _port: &str,
+    socket: &mut TcpStream,
+    ip: &str,
+    port: &str,
     repo_local: &str,
 ) -> Result<String, CommandsError> {
     // Obtengo el repositorio remoto
@@ -55,11 +58,27 @@ pub fn git_push(
         Ok(rfs) => rfs,
         Err(_) => return Err(CommandsError::PullCurrentBranchNotFound),
     };
-    let git_config = GitConfig::new_from_file(repo_local)?;
+    let _git_config = GitConfig::new_from_file(repo_local)?;
     
     // Codear esto que lo necesita tambien el fetch
     // El remoto debe ser dinamico no estatico, y no debe haber remotos repetidos
-    // let remote_url = git_config.get_remote_url(current_rfs.get_name());
+    // let repo_remoto = git_config.get_remote_url(current_rfs.get_name());
+    
+    // Valido si la branch existe en el repositorio local
+    let name_branch = current_rfs.get_name();
+    let rfs_fetch = format!("refs/heads/{}", name_branch);
+    let path_complete = format!("{}/.git/{}", repo_local, rfs_fetch);
+    if !Path::new(&path_complete).exists() {
+        return Ok("No hay cambios para hacer push".to_string());
+    }
+
+    // Prepara la solicitud "git-upload-pack" para el servidor
+    let repo_remoto = "a codear...";
+    let message =
+        GitRequest::generate_request_string(RequestCommand::ReceivePack, repo_remoto, ip, port);
+    
+    let server = reference_discovery(socket, message, repo_remoto, &Vec::new())?;
+    println!("Server: {:?}", server);
 
 
     Ok("Hola, soy baby push!".to_string())
