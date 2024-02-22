97acc30a53138e2eeb7fed5582094ecf43f6a0c6
Brayan Ricaldi <brayan.r.cool@gmail.com>
2024-02-22 12:00:34 -0300

    Create PRInfo

---
+++ git/src/commands/pull_request.rs
@@ -0,0 +1,23 @@
+pub struct PRInfo {
+    pub owner: String,
+    pub repo: String,
+    pub title: String,
+    pub body: String,
+    pub head: String,
+    pub base: String,
+}
+
+impl PRInfo {
+    pub fn new(title: &str, body: &str, head: &str, base: &str) -> PRInfo {
+        let owner = "owner";
+        let repo = "repo";
+        PRInfo {
+            owner: owner.to_string(),
+            repo: repo.to_string(),
+            title: title.to_string(),
+            body: body.to_string(),
+            head: head.to_string(),
+            base: base.to_string(),
+        }
+    }
+}
