0e1b6d888fb220e81a54647a3ee7763d94ed9dde
Brayan Ricaldi <brayan.r.cool@gmail.com>
2024-07-12 22:10:27 -0300

    Add documentation

--- git/bin/server.rs
+++ git/bin/server.rs
@@ -162,16 +162,57 @@ fn handle_client_http(
     Ok(())
 }
 
+/// Inicializa la configuración del programa a partir de los argumentos de línea de comandos.
+///
+/// # Returns
+///
+/// Retorna un `Result` que contiene la configuración inicializada si es exitosa, o un 
+/// `GitError` si falla.
+///
+/// # Errors
+///
+/// Puede fallar si hay errores al procesar los argumentos del archivo config.
+///
 fn initialize_config() -> Result<Config, GitError> {
     let args: Vec<String> = env::args().collect();
     Config::new(args)
 }
 
+/// Crea y devuelve un `TcpListener` para un servidor escuchando en la dirección especificada por `ip` y `port`.
+///
+/// # Arguments
+///
+/// * `ip` - La dirección IP donde se iniciará el servidor.
+/// * `port` - El puerto en el que escuchará el servidor, representado como una cadena.
+///
+/// # Returns
+///
+/// Retorna un `Result` que contiene un `TcpListener` si la creación es exitosa, o un `GitError` si falla.
+///
+/// # Errors
+///
+/// Puede fallar si hay errores al intentar iniciar el servidor en la dirección y puerto especificados.
+///
 fn create_listener(ip: &str, port: &String) -> Result<TcpListener, GitError> {
     let address = format!("{}:{}", ip, port);
     Ok(start_server(&address)?)
 }
 
+/// Inicia el logging en un hilo separado, escribiendo en el archivo de log especificado por `path_log`.
+///
+/// # Arguments
+///
+/// * `path_log` - La ruta al archivo de log donde se escribirán los registros.
+///
+/// # Returns
+///
+/// Retorna un `Result` que contiene una tupla con un `Arc<Mutex<Sender<String>>>` para transmitir mensajes de log
+/// y un `JoinHandle<()>` que representa el handle del hilo de logging.
+///
+/// # Errors
+///
+/// Puede fallar si hay errores al intentar iniciar el hilo de logging o al abrir el archivo de log.
+///
 fn start_logging(path_log: String) -> Result<(Arc<Mutex<Sender<String>>>, JoinHandle<()>), GitError> {
     let (tx, rx) = mpsc::channel();
     let shared_tx = Arc::new(Mutex::new(tx));
@@ -181,6 +222,27 @@ fn start_logging(path_log: String) -> Result<(Arc<Mutex<Sender<String>>>, JoinHa
     Ok((shared_tx, log_handle))
 }
 
+/// Inicia un hilo para manejar conexiones entrantes en un servidor TCP, utilizando un 
+/// `TcpListener` dado.
+///
+/// # Arguments
+///
+/// * `listener` - El `TcpListener` que acepta conexiones entrantes.
+/// * `shared_tx` - Un `Arc<Mutex<Sender<String>>>` para transmitir mensajes de log.
+/// * `src` - La ruta del directorio raíz para operaciones de servidor.
+/// * `handler` - Una función que maneja cada conexión entrante.
+///
+/// # Returns
+///
+/// Retorna un `Result` que contiene un `JoinHandle<()>` que representa el handle del hilo de 
+/// servidor,
+/// o un `GitError` si falla.
+///
+/// # Errors
+///
+/// Puede fallar si hay errores al manejar conexiones entrantes o al ejecutar el handler de 
+/// conexión.
+///
 fn start_server_thread(
     listener: TcpListener,
     shared_tx: Arc<Mutex<Sender<String>>>,
@@ -193,6 +255,18 @@ fn start_server_thread(
     Ok(handle)
 }
 
+/// Espera a que finalicen los hilos de logging, servidor de daemon y servidor HTTP.
+///
+/// # Arguments
+///
+/// * `log_handle` - El handle del hilo de logging.
+/// * `daemon_handle` - El handle del hilo del servidor de daemon.
+/// * `http_handle` - El handle del hilo del servidor HTTP.
+///
+/// # Panics
+///
+/// Puede generar un pánico si alguno de los hilos no finaliza correctamente.
+///
 fn wait_for_threads(log_handle: JoinHandle<()>, daemon_handle: JoinHandle<()>, http_handle: JoinHandle<()>) {
     log_handle.join().expect("No se pudo escribir el archivo de log");
     daemon_handle.join().expect("No hay clientes en git-daemon");
