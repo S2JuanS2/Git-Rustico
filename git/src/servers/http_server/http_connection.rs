2f196ae9574d2a925bc05349c667586131db5495
Brayan Ricaldi <brayan.r.cool@gmail.com>
2024-07-18 12:52:33 -0300

    Improve the http logger

--- git/src/servers/http_server/http_connection.rs
+++ git/src/servers/http_server/http_connection.rs
@@ -2,6 +2,7 @@ use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::Sender;
 use crate::errors::GitError;
+use crate::util::logger::{get_client_signature, log_client_connect};
 // use crate::servers::errors::ServerError;
 use super::http_request::HttpRequest;
 
@@ -11,6 +12,9 @@ pub fn handle_client_http(
     tx: Arc<Mutex<Sender<String>>>,
     root_directory: String,
 ) -> Result<(), GitError> {
+    // Loggear la conexión del cliente
+    log_client_connect(stream, &tx);
+    let _signature = get_client_signature(stream)?;
     // Creo la solicitud HTTP
     let request = HttpRequest::new_from_reader(stream)?;
     // Manejar la solicitud HTTP
